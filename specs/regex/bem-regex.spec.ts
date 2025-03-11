a0600fc216ec0640b3c68f3434e33f52030f90c1
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-03-11 14:51:09 +0300

    refactor bem-regex.spec.ts
    Signed-off-by: nikkeyl <131481562+nikkeyl@users.noreply.github.com>

--- specs/regex/bem-regex.spec.ts
+++ specs/regex/bem-regex.spec.ts
@@ -2,48 +2,46 @@ import { describe, expect, test as spec } from 'bun:test';
 
 import { CSSRules } from '#rules/css.ts';
 
+const BEMClassRegex = new RegExp(`${CSSRules['selector-class-pattern'][0]}`);
+const validClasses = [
+  'block',
+  'block-name',
+  'block-name-123',
+  'block--modifier',
+  'block-name--modifier-name',
+  'block-name--modifier',
+  'block--modifier-name',
+  'block__element',
+  'block-name__element-name',
+  'block-name__element',
+  'block__element-name',
+  'block__element--modifier',
+  'block-name__element-name--modifier-name',
+  'block-name__element-name--modifier',
+  'block-name__element--modifier-name',
+  'block__element-name--modifier-name',
+];
+const invalidClasses = [
+  'block__element_modifier',
+  'Block',
+  'BlockElement',
+  '123',
+];
+
 describe('BEM Class Regex', () => {
-  const BEMClassRegex = new RegExp(`${CSSRules['selector-class-pattern'][0]}`);
-
   describe('Valid BEM classes', () => {
-    const validClasses = [
-      'block',
-      'block-name',
-      'block-name-123',
-      'block--modifier',
-      'block-name--modifier-name',
-      'block-name--modifier',
-      'block--modifier-name',
-      'block__element',
-      'block-name__element-name',
-      'block-name__element',
-      'block__element-name',
-      'block__element--modifier',
-      'block-name__element-name--modifier-name',
-      'block-name__element-name--modifier',
-      'block-name__element--modifier-name',
-      'block__element-name--modifier-name',
-    ];
-
-    for (const className of validClasses) {
+    validClasses.forEach((className) => {
       spec(`should accept "${className}"`, () => {
         expect(className).toMatch(BEMClassRegex);
       });
-    }
+    });
   });
 
   describe('Invalid BEM classes', () => {
-    const invalidClasses = [
-      'block__element_modifier',
-      'Block',
-      'BlockElement',
-      '123',
-    ];
-
-    for (const className of invalidClasses) {
+    validClasses.forEach((className) => {
       spec(`should reject "${className}"`, () => {
         expect(className).not.toMatch(BEMClassRegex);
       });
-    }
+    });
   });
 });
